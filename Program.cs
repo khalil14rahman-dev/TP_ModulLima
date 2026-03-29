99dee8859bdc3a0419579824aab3f3cc7e39068a
khalilgtgbgtkhalilgtg@gmail.com <khaaleell>
2026-03-29 17:32:10 +0700

    Final update: combined all task features

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class HaloGeneric
+{
+    // Method Generic SapaUser
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    // Konstruktor
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    // Method PrintData
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // --- Output dari branch generic-method ---
+        HaloGeneric halo = new HaloGeneric();
+        // Input berupa nama panggilan praktikan (Poin 4.C)
+        halo.SapaUser("Khalil");
+
+        // --- Output dari branch generic-class ---
+        // Input berupa NIM praktikan (Poin 6.C)
+        DataGeneric<string> dataNIM = new DataGeneric<string>("103082400033");
+        dataNIM.PrintData();
+
+        // Menjaga console tetap terbuka
+        Console.WriteLine("\nTekan tombol apa saja untuk keluar...");
+        Console.ReadKey();
+    }
+}
--- obj/Debug/net10.0/TP_ModulLima_103082400033.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_ModulLima_103082400033.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_ModulLima_103082400033")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85c772808afbe5e118858eaaccf3630f8d9a505")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_ModulLima_103082400033")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_ModulLima_103082400033")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
